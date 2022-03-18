4630b7ad31dcd4c2d49e689b3299c30df2ed97b2
Natalia <alimadeoliveiranatalia@gmail.com>
2022-03-18 20:56:18 -0300

    definindo rotas de create e list

--- src/routes/assuntos.routes.ts
+++ src/routes/assuntos.routes.ts
@@ -1,10 +1,15 @@
-import { request, response, Router } from "express";
+import { Router } from "express";
 import { createAssuntoController } from "../modules/assunto/useCases/createAssunto";
+import { listAllAssuntosController } from "../modules/assunto/useCases/listAssunto";
 
 const assuntosRoutes = Router();
 
 assuntosRoutes.post("/", (request, response) => 
     createAssuntoController.handle(request, response)
+);
+
+assuntosRoutes.get("/", (request, response) =>
+    listAllAssuntosController.handle(request, response)
 );
 
 export { assuntosRoutes };
