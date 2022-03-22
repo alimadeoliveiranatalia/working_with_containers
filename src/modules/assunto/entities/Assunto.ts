b7a2c33a9cb27ead962268c218e469ab8c39caec
Natalia <alimadeoliveiranatalia@gmail.com>
2022-03-22 02:08:04 -0300

    adcionando decoradores

--- src/modules/assunto/entities/Assunto.ts
+++ src/modules/assunto/entities/Assunto.ts
@@ -1,8 +1,15 @@
+import { Column, CreateDateColumn, Entity, PrimaryColumn } from "typeorm";
 import { v4 as uuidv4 } from "uuid";
 
+@Entity("assuntos")
 class Assunto {
+    @PrimaryColumn()
     cod_assunto: string;
+
+    @Column()
     assunto!: string;
+
+    @CreateDateColumn()
     created_at!: Date;
 
     constructor(){
--- src/modules/autor/entities/Autor.ts
+++ src/modules/autor/entities/Autor.ts
@@ -1,9 +1,18 @@
+import { Column, CreateDateColumn, Entity, PrimaryColumn } from "typeorm";
 import { v4 as uuidv4 } from "uuid";
 
+@Entity("assuntos")
 class Autor {
+    @PrimaryColumn()
     cod_autor: string;
+
+    @Column()
     nome_autor!: string;
+
+    @CreateDateColumn()
     created_at!: Date;
+
+    @CreateDateColumn()
     updated_at!: Date;
 
     constructor(){
