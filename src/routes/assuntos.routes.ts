91f4808d62f797ebe3d19cb7b9673aa316a7ef48
Natalia <alimadeoliveiranatalia@gmail.com>
2022-03-26 23:22:47 -0300

    Ajustando routes

--- src/routes/assuntos.routes.ts
+++ src/routes/assuntos.routes.ts
@@ -1,15 +1,15 @@
 import { Router } from "express";
-import { createAssuntoController } from "../modules/assunto/useCases/createAssunto";
-import { listAllAssuntosController } from "../modules/assunto/useCases/listAssunto";
+import  createAssuntoController  from "../modules/livro/useCases/createAssunto";
+import  listAllAssuntosController  from "../modules/livro/useCases/listAssunto";
 
 const assuntosRoutes = Router();
 
 assuntosRoutes.post("/", (request, response) => 
-    createAssuntoController.handle(request, response)
+    createAssuntoController().handle(request, response)
 );
 
 assuntosRoutes.get("/", (request, response) =>
-    listAllAssuntosController.handle(request, response)
+    listAllAssuntosController().handle(request, response)
 );
 
 export { assuntosRoutes };
