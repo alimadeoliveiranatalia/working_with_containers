b1438635db6877a9410f5343c8fcf07b83d39ca3
Natalia <alimadeoliveiranatalia@gmail.com>
2022-03-30 08:21:43 -0300

    Camuflando parametros de conexao

--- src/modules/livro/entities/Editora.ts
+++ src/modules/livro/entities/Editora.ts
@@ -1,8 +1,11 @@
+import { Column, Entity, PrimaryColumn } from "typeorm";
 import { v4 as uuid } from "uuid";
-
+@Entity("tb_editora")
 export class Editora {
+    @PrimaryColumn()
     cod_editora:string;
-
+    
+    @Column()
     name_editora!: string;
 
     constructor(){
