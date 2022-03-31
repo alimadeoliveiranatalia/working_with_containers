3ce77c6daf373542ad8deb9bce516b8619e0f17b
Natalia <alimadeoliveiranatalia@gmail.com>
2022-03-31 01:30:14 -0300

    Add decoration Entity

--- src/modules/livro/entities/Autor.ts
+++ src/modules/livro/entities/Autor.ts
@@ -1,8 +1,17 @@
+import { Column, CreateDateColumn, Entity, PrimaryGeneratedColumn } from "typeorm";
 import { v4 as uuidv4 } from "uuid";
+@Entity("tb_autor")
 class Autor {
+    @PrimaryGeneratedColumn()
     id: string;
+
+    @Column()
     name!: string;
+
+    @CreateDateColumn()
     created_at!: Date;
+
+    @CreateDateColumn()
     updated_at!: Date;
 
     constructor(){
